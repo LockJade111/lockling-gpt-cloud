e1f1850c903453e3e6b32246bd0380d57f670864
LockJade <lockjade1@LockJadedeMac-mini.local>
2025-05-30 23:34:52 -0700

    🚀 Dev: 自动提交所有更新 @ 2025-05-30 23:34:52

--- check_permission.py
+++ check_permission.py
@@ -7,6 +7,7 @@ load_dotenv()
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_SERVICE_ROLE_KEY")
+SUPER_SECRET_KEY = "🧪_default_fake_key_for_dev_mode"
 
 headers = {
     "apikey": SUPABASE_KEY,
